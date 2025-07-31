517109f0086e500d85517a140dfcd7bf3bb2f9c9
Norbert Deák <thedeaknorbert@gmail.com>
2025-07-31 10:33:40 +0200

    Implement DataHandler class

--- src/data/data_handler.py
+++ src/data/data_handler.py
@@ -1,3 +1,4 @@
+from src.data.interfaces.data_interface import DataInterface
 from src.data.data_loader import DataLoader
 
 
@@ -6,4 +7,54 @@ class DataHandler:
     This class preprocesses the data for use.
     """
     def __init__(self, data_loader: DataLoader):
-        pass
+        """
+        Constructor. We create the following data structures:
+        - time_series: Pandas DataFrame containing all water level time series data.
+        The indices are dates and the column names are station regional numbers.
+        - meta: Pandas DataFrame containing regional numbers, station names and kilometers.
+        - gauges: List of gauges (regional numbers).
+        - station_info: Dictionary: keys are regional numbers, values are dictionaries like
+        {'life_interval': {'start': '1876-01-01', 'end': '2019-12-31'}, 'null_point': 73.7, 'level_group': 570}
+        (for Szeged)
+        :param DataLoader data_loader: a DataLoader instance
+        """
+        self.data_if = DataInterface()
+
+        self.run(data_loader)
+
+    def run(self, data_loader: DataLoader) -> None:
+        """
+        Run function. Gets all data structures described in the constructor.
+        :param DataLoader data_loader: a DataLoader instance
+        """
+        gauges = data_loader.meta_data.index.tolist()
+
+        data = {
+            'time_series': data_loader.measurement_data,
+            'meta': data_loader.meta_data,
+            'gauges': gauges,
+            'station_info': self.get_station_info(data_loader, gauges)
+        }
+
+        self.data_if = DataInterface(data)
+
+    @staticmethod
+    def get_station_info(data_loader: DataLoader, gauges: list) -> dict:
+        """
+        We create a dictionary by combining dictionaries.
+        :param DataLoader data_loader: a DataLoader instance
+        :param list gauges: list of gauges (regional numbers)
+        :return dict: dictionary of relevant station information
+        """
+        station_info = dict()
+
+        for gauge in gauges:
+            reg_number = str(gauge)
+
+            station_info[reg_number] = {
+                "life_interval": data_loader.station_lifetimes.get(reg_number),
+                "null_point": data_loader.null_points.get(reg_number),
+                "level_group": data_loader.level_groups.get(reg_number)
+            }
+
+        return station_info

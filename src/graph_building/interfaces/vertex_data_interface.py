788343dd03e43b58b0579ec2a115df9b5cf39d7e
Norbert Deák <thedeaknorbert@gmail.com>
2025-08-10 16:47:51 +0200

    Implement VertexDataInterface class

--- src/graph_building/interfaces/vertex_data_interface.py
+++ src/graph_building/interfaces/vertex_data_interface.py
@@ -2,5 +2,17 @@ class VertexDataInterface:
     """
     Class for storing data created in GraphDataGenerator and used in analysis.
     """
-    def __init__(self):
-        pass
+    def __init__(self, data: dict = None):
+        """
+        Constructor.
+        :param dict data: Dictionary: the keys represent the data structures.
+        The expected keys are:
+        - 'vertices'
+        - 'river_kms'
+        """
+        self.vertices = dict()
+        self.river_kms = list()
+
+        if data is not None:
+            for key, value in data.items():
+                setattr(self, key, value)

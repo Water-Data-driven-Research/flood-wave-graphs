b2f40840f3319e551ed23e8de7a5064029435f70
Norbert Deák <thedeaknorbert@gmail.com>
2025-08-12 17:16:42 +0200

    Move finding delta-peaks to separate function and remove date filtering

--- src/graph_building/graph_data_generator.py
+++ src/graph_building/graph_data_generator.py
@@ -11,9 +11,7 @@ class GraphDataGenerator:
     def __init__(self,
                  data_interface: DataInterface,
                  beta: int,
-                 delta: int = 2,
-                 start_date: str = '1876-01-01',
-                 end_date: str = '2019-12-31'
+                 delta: int = 2
                  ):
         """
         Constructor.
@@ -22,15 +20,11 @@ class GraphDataGenerator:
         :param int delta: the number of days that a record is required to be greater
                           than the records before, and to be greater or equal to after
                           to be considered a peak
-        :param str start_date: the beginning date of our analysis
-        :param str end_date: the end date of our analysis
         """
         self.data_interface = data_interface
 
         self.beta = beta
         self.delta = delta
-        self.start_date = start_date
-        self.end_date = end_date
 
         self.vertex_interface = VertexDataInterface()
 
@@ -61,19 +55,33 @@ class GraphDataGenerator:
         :return dict: dictionary of potential vertices
         """
         station_info = self.data_interface.station_info
-        start_date, end_date = self.find_time_interval(
-            start_dates=(
-                station_info[gauge]['life_interval']['start'],
-                self.start_date
-            ),
-            end_dates=(
-                station_info[gauge]['life_interval']['end'],
-                self.end_date
-            )
-        )
+
+        start_date = station_info[gauge]['life_interval']['start']
+        end_date = station_info[gauge]['life_interval']['end']
 
         series = self.data_interface.time_series[gauge].loc[start_date:end_date]
 
+        peak_series = self.get_peaks(series=series)
+
+        null_point = station_info[gauge]['null_point']
+        level_group = station_info[gauge]['level_group']
+
+        peak_data = {
+            date: {
+                'value': round(value + null_point, 2),
+                'color': 'yellow' if value < level_group else 'red'
+            }
+            for date, value in peak_series.items()
+        }
+
+        return peak_data
+
+    def get_peaks(self, series: pd.Series) -> pd.Series:
+        """
+        We find the delta-peaks, and return the filtered series.
+        :param pd.Series series: the original time series
+        :return pd.Series: found delta-peaks
+        """
         before_max = series.rolling(
             window=self.delta + 1,
             min_periods=self.delta + 1
@@ -89,35 +97,4 @@ class GraphDataGenerator:
 
         is_peak = cond_before & cond_after
 
-        null_point = station_info[gauge]['null_point']
-        level_group = station_info[gauge]['level_group']
-
-        peak_data = {
-            date: {
-                'value': round(value + null_point, 2),
-                'color': 'yellow' if value < level_group else 'red'
-            }
-            for date, value in series[is_peak].items()
-        }
-
-        return peak_data
-
-    @staticmethod
-    def find_time_interval(start_dates: tuple,
-                           end_dates: tuple
-                           ) -> tuple:
-        """
-        Helper function for finding the intersection of time intervals.
-        :param tuple start_dates: tuple of start dates
-        :param tuple end_dates: tuple of end dates
-        :return tuple: final start and end dates
-        """
-        start_date = max(
-            pd.to_datetime(date) for date in start_dates
-        ).strftime('%Y-%m-%d')
-
-        end_date = min(
-            pd.to_datetime(date) for date in end_dates
-        ).strftime('%Y-%m-%d')
-
-        return start_date, end_date
+        return series[is_peak]

858f7e0581fb2e5ec2e9544c2e654e80373115b5
Norbert Deák <thedeaknorbert@gmail.com>
2025-08-23 13:12:36 +0200

    Delete GraphBuilder class

--- src/graph_building/graph_builder.py
+++
@@ -1,9 +0,0 @@
-from src.graph_building.interfaces.graph_data_interface import GraphDataInterface
-
-
-class GraphBuilder:
-    """
-    This class creates the FWG.
-    """
-    def __init__(self, graph_data_interface: GraphDataInterface):
-        pass

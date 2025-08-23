6a3ac64839a3c24205cf173432db1e6823cc87f9
Norbert Deák <thedeaknorbert@gmail.com>
2025-08-23 13:31:35 +0200

    Remove unimplemented parameter from EdgeInterface

--- src/graph_building/interfaces/edge_interface.py
+++ src/graph_building/interfaces/edge_interface.py
@@ -2,5 +2,5 @@ class EdgeInterface:
     """
     Class for storing edges used in the creation of the FWG.
     """
-    def __init__(self, data: dict = None):
+    def __init__(self):
         pass

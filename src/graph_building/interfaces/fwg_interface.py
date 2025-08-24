d015f2ea9ceee0a213ed743a7d8ad3b0d47f7434
Norbert Deák <thedeaknorbert@gmail.com>
2025-08-24 19:14:52 +0200

    Implement FWGInterface

--- src/graph_building/interfaces/fwg_interface.py
+++ src/graph_building/interfaces/fwg_interface.py
@@ -1,6 +1,12 @@
+import networkx as nx
+
+
 class FWGInterface:
     """
     Class used to store the FWG.
     """
     def __init__(self):
-        pass
+        """
+        Constructor. The only member variable is the graph instance.
+        """
+        self.fwg = nx.DiGraph()

d07609f2034a2632fda0cb2f98409a31df8e5b42
Norbert Deák <thedeaknorbert@gmail.com>
2025-08-24 19:45:24 +0200

    Implement saving to pickle

--- src/data/generated_data_loader.py
+++ src/data/generated_data_loader.py
@@ -1,4 +1,32 @@
+import os
+import pickle
+
+import networkx as nx
+
+
 class GeneratedDataLoader:
     """
-    This class writes and reads pickle files.
+    Class for writing and reading pickle files.
     """
+    @staticmethod
+    def save_pickle(folder_path: str,
+                    folder_name: str,
+                    file_name: str,
+                    graph: nx.DiGraph
+                    ):
+        """
+        Method for saving a graph into a pickle file.
+        :param str folder_path: path of the data folder
+        :param str folder_name: name of the folder inside the generated folder
+        :param str file_name: name of the file
+        :param nx.DiGraph graph: a directed graph
+        """
+        os.makedirs(
+            os.path.join(folder_path, 'generated', folder_name),
+            exist_ok=True
+        )
+
+        with open(os.path.join(
+                folder_path, 'generated', folder_name, f'{file_name}.pkl'
+        ), 'wb') as f:
+            pickle.dump(graph, f)

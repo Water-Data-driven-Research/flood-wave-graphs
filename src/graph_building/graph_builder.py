c1d6b7540e2895232cc0c649347e07fd412e952f
Norbert Deák <thedeaknorbert@gmail.com>
2025-08-24 14:56:15 +0200

    Add EdgeFinder running to GraphBuilder

--- src/graph_building/graph_builder.py
+++ src/graph_building/graph_builder.py
@@ -1,5 +1,6 @@
 from src.data.interfaces.data_interface import DataInterface
 from src.graph_building.delta_peak_finder import DeltaPeakFinder
+from src.graph_building.edge_finder import EdgeFinder
 
 
 class GraphBuilder:
@@ -27,9 +28,14 @@ class GraphBuilder:
             data_interface=self.data_interface,
             delta=self.delta
         )
+        self.edge_finder = EdgeFinder(
+            gauges=self.data_interface.gauges,
+            beta=self.beta
+        )
 
     def run(self):
         """
         Runs the operations for building the graph.
         """
         self.delta_peak_finder.run()
+        self.edge_finder.run(vertex_interface=self.delta_peak_finder.vertex_interface)

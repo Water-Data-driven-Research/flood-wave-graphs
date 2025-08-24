25a33e9b0a6724a3374b32d5b6b19de8e429cd6e
Norbert Deák <thedeaknorbert@gmail.com>
2025-08-24 19:56:06 +0200

    Change pickle file folder structure

--- src/data/generated_data_loader.py
+++ src/data/generated_data_loader.py
@@ -10,23 +10,21 @@ class GeneratedDataLoader:
     """
     @staticmethod
     def save_pickle(folder_path: str,
-                    folder_name: str,
                     file_name: str,
                     graph: nx.DiGraph
                     ):
         """
         Method for saving a graph into a pickle file.
         :param str folder_path: path of the data folder
-        :param str folder_name: name of the folder inside the generated folder
         :param str file_name: name of the file
         :param nx.DiGraph graph: a directed graph
         """
         os.makedirs(
-            os.path.join(folder_path, 'generated', folder_name),
+            os.path.join(folder_path, 'generated'),
             exist_ok=True
         )
 
         with open(os.path.join(
-                folder_path, 'generated', folder_name, f'{file_name}.pkl'
+                folder_path, 'generated', f'{file_name}.pkl'
         ), 'wb') as f:
             pickle.dump(graph, f)

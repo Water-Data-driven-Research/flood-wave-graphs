93d5bec81b5267c97bee68fe6009016683d6478f
Norbert Deák <thedeaknorbert@gmail.com>
2025-08-24 19:19:59 +0200

    Implement graph building

--- src/graph_building/graph_builder.py
+++ src/graph_building/graph_builder.py
@@ -1,6 +1,9 @@
+import networkx as nx
+
 from src.data.interfaces.data_interface import DataInterface
 from src.graph_building.delta_peak_finder import DeltaPeakFinder
 from src.graph_building.edge_finder import EdgeFinder
+from src.graph_building.interfaces.fwg_interface import FWGInterface
 
 
 class GraphBuilder:
@@ -33,9 +36,32 @@ class GraphBuilder:
             beta=self.beta
         )
 
+        self.fwg_interface = FWGInterface()
+
     def run(self):
         """
         Runs the operations for building the graph.
         """
         self.delta_peak_finder.run()
         self.edge_finder.run(vertex_interface=self.delta_peak_finder.vertex_interface)
+
+        self.fwg_interface.fwg = self.build_graph()
+
+    def build_graph(self) -> nx.DiGraph:
+        """
+        We create the directed graph.
+        :return nx.DiGraph: the FWG
+        """
+        fwg = nx.DiGraph()
+
+        edges = self.edge_finder.edge_interface.edges
+
+        final_edges = []
+        for gauge_pair, date_pairs in edges.items():
+            start_gauge, end_gauge = gauge_pair.split("-")
+            for start_date, end_date in date_pairs:
+                final_edges.append(((start_gauge, start_date), (end_gauge, end_date)))
+
+        fwg.add_edges_from(final_edges)
+
+        return fwg

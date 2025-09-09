c7f2a127299edef096b6d91c64b9d56a68c399da
Norbert Deák <thedeaknorbert@gmail.com>
2025-09-09 13:01:40 +0200

    Add method for loading pickle files (#34)
    * Implement read_pickle() method
    
    * Add missing annotation

--- src/data/generated_data_loader.py
+++ src/data/generated_data_loader.py
@@ -28,3 +28,20 @@ class GeneratedDataLoader:
                 folder_path, 'generated', f'{file_name}.pkl'
         ), 'wb') as f:
             pickle.dump(graph, f)
+
+    @staticmethod
+    def read_pickle(folder_path: str,
+                    file_name: str,
+                    ) -> nx.DiGraph:
+        """
+        Method for loading a graph from a pickle file.
+        :param str folder_path: path of the target folder
+        :param str file_name: name of the file
+        :return nx.DiGraph: the loaded graph
+        """
+        with open(os.path.join(
+                folder_path, f'{file_name}.pkl'
+        ), 'rb') as f:
+            graph = pickle.load(f)
+
+        return graph

280db05a41a274d23b88578c96a49653a379a4e7
Norbert Deák <thedeaknorbert@gmail.com>
2025-09-13 16:29:06 +0200

    Add workflow and tests for flood wave extraction

---
+++ src/graph_manipulation/tests/test_wave_extract.py
@@ -0,0 +1,41 @@
+import networkx as nx
+import pytest
+
+from src.graph_manipulation.flood_wave_extractor import FloodWaveExtractor
+
+
+@pytest.fixture
+def mock_graph() -> nx.DiGraph:
+    graph = nx.DiGraph()
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('B', 'D'),
+        ('A', 'C'),
+        ('C', 'D')
+    ])
+    return graph
+
+
+@pytest.mark.parametrize('with_equivalence, expected_waves', [
+    (
+        False,
+        [['A', 'B', 'D'], ['A', 'C', 'D']]
+    ),
+    (
+        True,
+        [['A', 'B', 'D']]
+    )
+])
+def test_wave_extraction(mock_graph: nx.DiGraph,
+                         with_equivalence: bool,
+                         expected_waves: list
+                         ):
+    extractor = FloodWaveExtractor(fwg=mock_graph)
+    extractor(with_equivalence=with_equivalence)
+
+    waves = extractor.flood_wave_interface.flood_waves
+
+    waves_sorted = sorted(waves)
+    expected_sorted = sorted(expected_waves)
+
+    assert waves_sorted == expected_sorted

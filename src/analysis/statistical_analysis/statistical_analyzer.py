17a8d7e1adf4e37cd3e91a656fd4302cb442e38a
Norbert Deák <thedeaknorbert@gmail.com>
2025-09-20 15:41:12 +0200

    Implement calculation of propagation time statistics

--- src/analysis/statistical_analysis/statistical_analyzer.py
+++ src/analysis/statistical_analysis/statistical_analyzer.py
@@ -49,6 +49,52 @@ class StatisticalAnalyzer:
 
         return {'yearly': yearly, 'quarterly': quarterly}
 
+    def get_propagation_times(self,
+                              lower_station: float,
+                              upper_station: float,
+                              statistic: str = 'mean'
+                              ) -> dict:
+        """
+        Calculates selected statistic of wave propagation times
+        between two stations, yearly and quarterly.
+        :param float lower_station: the downstream station (river km)
+        :param float upper_station: the upstream station (river km)
+        :param str statistic: the statistic to calculate
+        :return dict: keys are time interval sizes, values are the respective data
+        """
+        flood_waves = self.get_filtered_waves(
+            lower_station=lower_station,
+            upper_station=upper_station
+        )
+
+        propagation_times = list()
+        start_dates = list()
+        for wave in flood_waves:
+            start_date = pd.to_datetime(wave[0][1])
+            end_date = pd.to_datetime(wave[-1][1])
+            start_dates.append(start_date)
+            propagation_times.append((end_date - start_date).days)
+
+        df = pd.DataFrame({
+            'date': start_dates,
+            'propagation_time': propagation_times
+        }).set_index('date')
+
+        match statistic:
+            case 'mean':
+                yearly = df.resample('YE').mean()
+                quarterly = df.resample('QE').mean()
+            case 'median':
+                yearly = df.resample('YE').median()
+                quarterly = df.resample('QE').median()
+            case _:
+                raise ValueError('Invalid statistic')
+
+        yearly.index = yearly.index.to_period('Y')
+        quarterly.index = quarterly.index.to_period('Q')
+
+        return {'yearly': yearly, 'quarterly': quarterly}
+
     def get_filtered_waves(self,
                            lower_station: float,
                            upper_station: float

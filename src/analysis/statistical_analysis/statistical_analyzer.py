eab56d7664a51b80ba865a63fa12e148c949c2bf
Norbert Deák <thedeaknorbert@gmail.com>
2025-10-01 17:44:52 +0200

    Add method for getting propagation time statistics of red waves

--- src/analysis/statistical_analysis/statistical_analyzer.py
+++ src/analysis/statistical_analysis/statistical_analyzer.py
@@ -143,3 +143,29 @@ class StatisticalAnalyzer:
             check_whole_wave=check_whole_wave
         )
         return self.get_flood_wave_count(red_waves)
+
+    def get_red_wave_propagation_time_stat(self,
+                                           flood_waves: list,
+                                           target_station: float,
+                                           statistic: str = 'mean',
+                                           check_whole_wave: bool = False
+                                           ) -> dict:
+        """
+        Calculates the chosen statistic for the propagation times of red waves
+        that impacted the target station. A flood wave is considered red if it had
+        a high water level at the target station (or throughout).
+        Data is aggregated yearly and quarterly.
+        :param list flood_waves: list of flood waves to analyze
+        :param float target_station: the station to filter for
+        :param str statistic: the statistic to calculate (mean, median, etc.)
+        :param bool check_whole_wave: True if we require all nodes in the wave to be red,
+                                      False if we only consider the one at the target station
+        :return dict: keys are time interval sizes, values are the respective data
+        """
+        red_waves = FloodWaveFilter.get_red_waves(
+            flood_waves=flood_waves,
+            vertex_interface=self.vertex_interface,
+            target_station=str(target_station),
+            check_whole_wave=check_whole_wave
+        )
+        return self.get_propagation_time_stat(red_waves, statistic=statistic)

003583cc6d9de22551e0fda1af7a431e5d0b0751
Norbert Deák <thedeaknorbert@gmail.com>
2025-10-03 17:36:09 +0200

    Change boolean variable name

--- src/analysis/statistical_analysis/statistical_analyzer.py
+++ src/analysis/statistical_analysis/statistical_analyzer.py
@@ -124,7 +124,7 @@ class StatisticalAnalyzer:
     def get_red_wave_count_at_station(self,
                                       flood_waves: list,
                                       target_station: float,
-                                      check_whole_wave: bool = False
+                                      is_full_wave_considered: bool = False
                                       ) -> dict:
         """
         Calculates the number of red waves that impacted the target station.
@@ -133,15 +133,15 @@ class StatisticalAnalyzer:
         Data is aggregated yearly and quarterly.
         :param list flood_waves: list of flood waves to analyze
         :param float target_station: the station to filter for
-        :param bool check_whole_wave: True if we require all nodes in the wave to be red,
-                                      False if we only consider the one at the target station
+        :param bool is_full_wave_considered: True if we require all nodes in the wave to be red,
+                                             False if we only consider the one at the target station
         :return dict: keys are time interval sizes, values are the respective data
         """
         red_waves = FloodWaveFilter.get_red_waves(
             flood_waves=flood_waves,
             vertex_interface=self.vertex_interface,
             target_station=str(target_station),
-            check_whole_wave=check_whole_wave
+            is_full_wave_considered=is_full_wave_considered
         )
         return self.get_flood_wave_count(red_waves)
 
@@ -149,7 +149,7 @@ class StatisticalAnalyzer:
                                            flood_waves: list,
                                            target_station: float,
                                            statistic: str = 'mean',
-                                           check_whole_wave: bool = False
+                                           is_full_wave_considered: bool = False
                                            ) -> dict:
         """
         Calculates the chosen statistic for the propagation times of red waves
@@ -159,14 +159,14 @@ class StatisticalAnalyzer:
         :param list flood_waves: list of flood waves to analyze
         :param float target_station: the station to filter for
         :param str statistic: the statistic to calculate (mean, median, etc.)
-        :param bool check_whole_wave: True if we require all nodes in the wave to be red,
-                                      False if we only consider the one at the target station
+        :param bool is_full_wave_considered: True if we require all nodes in the wave to be red,
+                                             False if we only consider the one at the target station
         :return dict: keys are time interval sizes, values are the respective data
         """
         red_waves = FloodWaveFilter.get_red_waves(
             flood_waves=flood_waves,
             vertex_interface=self.vertex_interface,
             target_station=str(target_station),
-            check_whole_wave=check_whole_wave
+            is_full_wave_considered=is_full_wave_considered
         )
         return self.get_propagation_time_stat(red_waves, statistic=statistic)

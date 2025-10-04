9587283aa01ffc529b1f1c25d93836b8216d6a42
Norbert Deák <thedeaknorbert@gmail.com>
2025-10-04 10:46:20 +0200

    Add class for slope analysis

---
+++ src/analysis/statistical_analysis/slope_analyzer.py
@@ -0,0 +1,11 @@
+class SlopeAnalyzer:
+    """
+    This class is responsible for the analysis of slope values.
+    """
+    @staticmethod
+    def get_slope_distribution():
+        pass
+
+    @staticmethod
+    def get_slope_error_ratios_between_stations():
+        pass
